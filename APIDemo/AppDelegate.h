214f85db37ca0c3656a2282dbbf91f7a033937d5
mrkingchan <1203710130@qq.com>
2018-06-13 16:11:53 +0800

    Initial Commit

---
+++ APIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APIDemo
+//
+//  Created by Macx on 2018/6/13.
+//  Copyright © 2018年 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APIDemo
+//
+//  Created by Macx on 2018/6/13.
+//  Copyright © 2018年 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
